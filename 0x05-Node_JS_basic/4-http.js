ed912661bf54d86ac329fa7bb01f8fd27a926de4
lethurh <gugulethupraxedis@gmail.com>
2024-11-26 20:42:01 +0000

    In a file named 4-http.js,

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,12 +1,10 @@
 const http = require('http');
 
-const port = 1245;
-
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
 });
-app.listen(port);
+
+app.listen(1245);
 
 module.exports = app;
