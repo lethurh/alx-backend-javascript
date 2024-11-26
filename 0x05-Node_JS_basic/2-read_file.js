266ca46f49e9278f9345a9ece03a752f322a1c83
lethurh <gugulethupraxedis@gmail.com>
2024-11-26 20:08:35 +0000

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,63 +1,38 @@
 const fs = require('fs');
-function countStudents (path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8').toString().split('\n');
-    const students = data.slice(1, data.length - 1);
-    console.log(`Number of students: ${students.length - 1}`);
-    students.shift();
-    const subjects = {};
-    for (const row of students) {
-        const student = row.split(',');
-        if (!subjects[student[3]]) subjects[student[3]] = [];
-        subjects[student[3]].push(student[0]);
-    }
-    for (const subject in subjects) {
-        if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
-    }
-    // const cs = students.filter((student) => student.includes('CS'));
-    // const swe = students.filter((student) => student.includes('SWE'));
-    // console.log(`Number of students in CS: ${cs.length}. List: ${cs.map((student) => student.split(',')[0]).join(', ')}`);
-    // console.log(`Number of students in SWE: ${swe.length}. List: ${swe.map((student) => student.split(',')[0]).join(', ')}`);
 
-    // let cs = 0;
-    // let swe = 0;
-    // students.forEach((student) => student.includes('CS') ? cs++ : swe++);
-    // console.log(
-    //   `Number of students in CS: ${cs}. List: ${students
-    //     .filter((student) => student.includes('CS'))
-    //     .map((student) => student.split(',')[0])
-    //     .join(', ')}`
-    // );
-    // console.log(
-    //     `Number of students in SWE: ${swe}. List: ${students
-    //     .filter((student) => student.includes('SWE'))
-    //     .map((student) => student.split(',')[0])
-    //     .join(', ')}`
-    // );
-  } catch (err) {
+/**
+ * Reads CSV file containing student data, parses the data and
+ * prints it to stdout.
+ * @param {string} dbPath - path to csv file.
+ */
+function countStudents(dbPath) {
+  try {
+    let students = fs.readFileSync(dbPath, 'utf-8');
+    students = students.split('\n');
+    students = students.slice(1, students.length - 1);
+    const courses = new Map();
+
+    // Parse CSV data creating a map of courseData objects.
+    students.forEach((student) => {
+      const studentData = student.split(',');
+      const firstName = studentData[0];
+      const field = studentData[3];
+      if (courses.has(field)) {
+        courses.get(field).students.push(firstName);
+        courses.get(field).count += 1;
+      } else {
+        courses.set(field, { students: [firstName], count: 1 });
+      }
+    });
+
+    // Display information from map
+    console.log(Number of students: ${students.length});
+    courses.forEach((courseData, course) => {
+      console.log(Number of students in ${course}: ${courseData.count}. List: ${courseData.students.join(', ')});
+    });
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
-
-// const fs = require('fs');
-
-// const countStudents = (path) => {
-//   try {
-//     let data = fs.readFileSync(path, 'utf8').toString().split('\n');
-//     data = data.slice(1, data.length - 1);
-//     console.log(`Number of students: ${data.length}`);
-//     const subjects = {};
-//     for (const row of data) {
-//       const student = row.split(',');
-//       if (!subjects[student[3]]) subjects[student[3]] = [];
-//       subjects[student[3]].push(student[0]);
-//     }
-//     for (const subject in subjects) {
-//       if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
-//     }
-//   } catch (error) {
-//     throw new Error('Cannot load the database');
-//   }
-// };
-// module.exports = countStudents;
