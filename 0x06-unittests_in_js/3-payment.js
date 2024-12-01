2e11ad4efe20f3ba5fcbc1ce38dea35ed994700e
lethurh <gugulethupraxedis@gmail.com>
2024-12-01 10:22:26 +0000

    6. Async tests with done

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,10 +1,9 @@
+// 3-payment.js
 const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  // console.log(sum: ${sum}); // added console.log statement
-  // console.log(about to log The total is: ${sum}); // added console.log statement
-  console.log(The total is: ${sum});
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(The total is: ${result});
 }
 
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,19 +1,18 @@
-/**
- * sendPaymentRequestToAPi method test suite.
- */
+// 3-payment.test.js
 const sinon = require('sinon');
 const { expect } = require('chai');
 const Utils = require('./utils');
-const sendPaymentRequestToAPi = require('./3-payment');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function() {
-  describe('#Utils.calculateNumber()', function() {
-    it('should be called with args from sendPaymentRequestToApi', function() {
-      const spy = sinon.spy(Utils, 'calculateNumber');
-      sendPaymentRequestToAPi(100, 20);
-      expect(spy.calledOnce).to.equal(true);
-      expect(spy.calledWith('SUM', 100, 20)).to.equal(true);
-      spy.restore();
-    });
+describe('sendPaymentRequestToApi', function () {
+  it('should call Utils.calculateNumber with correct arguments', function () {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+
+    spy.restore(); // Always restore the spy after use
   });
 });
