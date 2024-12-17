cb34c3781b48b494762fefd5007cfb8abf5161a3
lethurh <gugulethupraxedis@gmail.com>
2024-12-17 14:14:10 +0000

    7. Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,12 +1,26 @@
 export default class Airport {
   constructor(name, code) {
-    if (typeof name !== 'string') throw TypeError('name must be a string');
-    if (typeof code !== 'string') throw TypeError('code must be a string');
-    this._name = name;
-    this._code = code;
+    this.name = name;
+    this.code = code;
   }
-// I don't know how to implement this part
-//   get [Symbol.toStringTag]() {
-//     return this._code;
-//   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
 }
