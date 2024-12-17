a14189cf301518afeea3773457a1edceec2fc8da
lethurh <gugulethupraxedis@gmail.com>
2024-12-17 17:25:07 +0000

    Hooks are useful functions that can be called before execute one or all tests in a suite

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,19 +1,20 @@
-/**
- * sendPaymentRequestToAPi method test suite.
- */
 const sinon = require('sinon');
-const { expect } = require('chai');
 const Utils = require('./utils');
-const sendPaymentRequestToAPi = require('./3-payment');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
-describe('sendPaymentRequestToApi', function() {
-  describe('#Utils.calculateNumber()', function() {
-    it('should be called with args from sendPaymentRequestToApi', function() {
-      const spy = sinon.spy(Utils, 'calculateNumber');
-      sendPaymentRequestToAPi(100, 20);
-      expect(spy.calledOnce).to.equal(true);
-      expect(spy.calledWith('SUM', 100, 20)).to.equal(true);
-      spy.restore();
-    });
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
+
+    dummy.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
   });
 });
